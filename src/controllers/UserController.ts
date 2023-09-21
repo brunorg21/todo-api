bbeba02081e7295dae2b41a14724dead7402447f
brunorg21 <rbrunog21@gmail.com>
2023-09-21 16:20:03 -0300

    feature: add new validations on login

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -8,21 +8,33 @@ export class UserController {
   async create(request: Request, response: Response) {
     const { name, email, password } = request.body;
 
-    const passwordHash = await hash(password, 8);
+    try {
+      const passwordHash = await hash(password, 8);
 
-    const user = await prismaClient.user.create({
-      data: {
-        name,
-        email,
-        password: passwordHash,
-      },
-    });
+      const user = await prismaClient.user.create({
+        data: {
+          name,
+          email,
+          password: passwordHash,
+        },
+      });
 
-    return response.status(200).json(user);
+      return response.status(200).json(user);
+    } catch (error) {
+      return response
+        .status(400)
+        .json({ message: "Não foi possível cadastrar!" });
+    }
   }
 
   async login(request: Request, response: Response) {
     const { email, password } = request.body;
+
+    if (!email || !password) {
+      return response
+        .status(404)
+        .json({ message: "Inserir todos os campos!!" });
+    }
 
     const user = await prismaClient.user.findUnique({
       where: {
@@ -31,13 +43,17 @@ export class UserController {
     });
 
     if (!user) {
-      return response.status(404).json({ message: "Invalid user or password" });
+      return response
+        .status(404)
+        .json({ message: "Email ou senha inválidos!" });
     }
 
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
-      return response.status(400).json({ message: "Invalid password" });
+      return response
+        .status(400)
+        .json({ message: "Email ou senha inválidos!" });
     }
 
     const token = generateToken(user.id);
